6f2384062c353bf07dd0a9e6b3419826d44bc1c6
liudandan <1064265199@qq.com>
2020-04-10 20:26:09 +0800

    添加slim

--- php/Creational/autoload.php
+++ php/Creational/autoload.php
@@ -1,8 +1,8 @@
 <?php
 function __autoload($className)
 {
-    $filename = __DIR__ . "/" . $className . ".php";
-    $filename = str_replace('\\', '/', $filename);
+    $filePath = __DIR__ . "/" . $className . ".php";
+    $filename = str_replace('\\', '/', $filePath);
     //is_readable() 函数判断指定文件名是否可读。
     if (is_readable($filename)) {
         require $filename;
