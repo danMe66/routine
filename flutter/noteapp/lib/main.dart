f4779dea84e06e4b2f56d3e318996df403d2fafe
liudandan <dawson2@qq.com>
2020-05-29 00:05:51 +0800

    note

---
+++ flutter/noteapp/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// main 函数调用了 runApp 方法，其功能是启动 Flutter应用
+// runApp 接受的是一个 widget 参数，MyApp 是一个对象，是 flutter应用 的一个根组件
+// void main() => runApp(MaterialApp(home: Text('Hello Word')));
+void main() => runApp(MaterialApp(home: Center(child: Text('Hello Word'))));
