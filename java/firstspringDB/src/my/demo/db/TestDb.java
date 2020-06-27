ae4e2432064baf26ec68e4bdafc1719fcd8d0a6f
liudandan <1064265199@qq.com>
2020-06-27 14:39:28 +0800

    spring mvc

--- java/firstspringDB/src/my/demo/db/TestDb.java
+++ java/firstspringDB/src/my/demo/db/TestDb.java
@@ -1,10 +1,20 @@
 package my.demo.db;
 
+import org.apache.ibatis.session.SqlSession;
+
+
 public class TestDb {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		try (SqlSession sqlSession = MyBatis.factory.openSession())
+		{
+			System.out.println("连接成功！");
+		} catch (Exception e)
+		{
+			System.err.println("连接异常！");
+		}
+		System.out.println("Exit");
 
 	}
 
