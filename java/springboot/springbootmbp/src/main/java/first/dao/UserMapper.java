a945bb97edf13fbbe0f608cb12bfcecfbe335077
liudandan <1064265199@qq.com>
2020-07-16 20:53:42 +0800

    mybatis-plus

---
+++ java/springboot/springbootmbp/src/main/java/first/dao/UserMapper.java
@@ -0,0 +1,9 @@
+package first.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import first.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ java/springboot/springbootmbp/src/test/java/first/selectTest.java
@@ -0,0 +1,26 @@
+package first;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import first.dao.UserMapper;
+import first.entity.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class selectTest {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	public void select() {
+		// 查询单条信息
+		User user = userMapper.selectById(1094590409767661570L);
+		System.err.println("查询结果：" + user);
+
+	}
+}
