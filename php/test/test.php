3fa3938c6d6e3e7b9c6181c43c976115e01765e3
liudandan <dawson2@qq.com>
2020-08-07 09:01:45 +0800

    init

--- php/test/test.php
+++ php/test/test.php
@@ -1,4 +1,18 @@
 <?php
-//echo "<pre>";
-$arr=[3,1,5,8];
-print_r($arr);
+class my_class {
+
+    var $my_value = [];
+    function my_class ($value){
+        $this->my_value[] = $value;
+    }
+    function set_value ($value){
+        $this->my_value = $value;
+    }
+
+}
+
+$a = new my_class('a');
+$a -> my_vulue[] = 'b';
+$a -> set_value('c');
+$a -> my_class('d');
+var_dump($a->my_value);
