e1a3e932e7d9e38d18601fee8588085850798467
liudandan <adam@123>
2021-12-14 22:29:26 +0800

    学习JavaScript的字符串和数组-提交保存

--- javascript/liaoxuefeng/section2/test1.js
+++ javascript/liaoxuefeng/section2/test1.js
@@ -63,3 +63,9 @@ lower; // 'hello'
 var s = 'hello, world';
 s.indexOf('world'); // 返回7
 s.indexOf('World'); // 没有找到指定的子串，返回-1
+
+//substring
+//substring()返回指定索引区间的子串：
+var s = 'hello, world'
+s.substring(0, 5); // 从索引0开始到5（不包括5），返回'hello'
+s.substring(7); // 从索引7开始到结束，返回'world'
