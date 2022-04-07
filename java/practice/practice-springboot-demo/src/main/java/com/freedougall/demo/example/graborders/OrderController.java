ae2bee0479cb4e354d06140782ed6a32a3323a7b
liudandan <1064265199@qq.com>
2022-04-07 17:54:49 +0800

    模拟抢单优化

--- java/practice/practice-springboot-demo/src/main/java/com/freedougall/demo/example/graborders/OrderController.java
+++ java/practice/practice-springboot-demo/src/main/java/com/freedougall/demo/example/graborders/OrderController.java
@@ -1,7 +1,9 @@
 package com.freedougall.demo.example.graborders;
 
+import com.freedougall.demo.common.untils.RedisUntil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,22 @@ import java.util.stream.IntStream;
 /**
  * redis分布式锁-模拟抢单
  * https://www.cnblogs.com/wangrudong003/p/10627539.html
+ * <p>
+ * 问题？：抢单不是可以多个用户同时抢的吗，这样的话只能一个一个的去抢，感觉这样设计有问题，可以用Redis DECR 原子性来操作
  *
  * @author liudandan
  */
 @Slf4j
-@RequestMapping("/api")
+@RestController
+@RequestMapping("/api/order")
 public class OrderController {
+
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Resource
+    private RedisUntil redisUntil;
+
     //总库存
     private long nKuCuen = 0;
     /**
@@ -29,9 +41,6 @@ public class OrderController {
     private String shangpingKey = "computer_key";
     //获取锁的超时时间 秒
     private int timeout = 30 * 1000;
-
-    @Resource
-    JedisCommon jedisCommon;
 
     @GetMapping("/qiangdan")
     public List<String> qiangdan() {
@@ -49,6 +58,7 @@ public class OrderController {
         nKuCuen = 10;
 
         //模拟开抢
+        //并行流模拟多用户抢购
         users.parallelStream().forEach(b -> {
             String shopUser = qiang(b);
             if (!StringUtils.isEmpty(shopUser)) {
@@ -70,12 +80,21 @@ public class OrderController {
         long startTime = System.currentTimeMillis();
 
         //未抢到的情况下，30秒内继续获取锁
+        //判断未抢成功的用户，timeout秒内继续获取锁
         while ((startTime + timeout) >= System.currentTimeMillis()) {
             //商品是否剩余
             if (nKuCuen <= 0) {
                 break;
             }
-            if (jedisCommon.setnx(shangpingKey, b)) {
+            /**
+             * 1：redis的普通key一般都允许覆盖，A用户set某个key后，B在set相同的key时同样能成功，
+             * 如果是锁场景，那就无法知道到底是哪个用户set成功的；这里的setNx方式为我们解决了这个问题，
+             * 简单原理是：当A用户先set成功了，那B用户set的时候就返回失败，满足了某个时间点只允许一个用户拿到锁。
+             * 2：锁过期时间：某个抢购场景时候，如果没有过期的概念，当A用户生成了锁，但是后面的流程被阻塞了一直无法释放锁，
+             * 那其他用户此时获取锁就会一直失败，无法完成抢购的活动；当然正常情况一般都不会阻塞，
+             * A用户流程会正常释放锁；过期时间只是为了更有保障。
+             */
+            if (redisUntil.setNx(shangpingKey, b, 10)) {
                 //用户b拿到锁
                 log.info("用户{}拿到锁...", b);
                 try {
@@ -101,9 +120,15 @@ public class OrderController {
                 } finally {
                     log.info("用户{}释放锁...", b);
                     //释放锁
-                    jedisCommon.delnx(shangpingKey, b);
+                    //获取锁后并下单成功，最后释放锁
+                    redisUntil.delNx(shangpingKey);
                 }
             } else {
+                /**
+                 * 上面是创建锁，同样的具有有效时间，但是我们不能完全依赖这个有效时间，
+                 * 场景如：有效时间设置1分钟，本身用户A获取锁后，没遇到什么特殊情况正常生成了抢购订单后，此时其他用户应该能正常下单了才对，
+                 * 但是由于有个1分钟后锁才能自动释放，那其他用户在这1分钟无法正常下单（因为锁还是A用户的），因此我们需要A用户操作完后，主动去解锁：
+                 */
                 //用户b没拿到锁，在超时范围内继续请求锁，不需要处理
                 if (b.equals("神牛-50") || b.equals("神牛-69")) {
                     log.info("用户{}等待获取锁...", b);
