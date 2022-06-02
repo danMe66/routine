32f18f2f3775e868b04c8573e83bd60203f77824
Dan dan <1064265199@qq.com>
2022-06-02 08:04:52 +0800

    update

--- flutter/study/my_app/lib/main.dart
+++ flutter/study/my_app/lib/main.dart
@@ -11,12 +11,37 @@ void main() {
   //   // home: SafeArea(child: MyScaffold()),
   //   home: TutorialHome(),
   // ));
+
   // 3:处理手势
+  // runApp(const MaterialApp(
+  //   home: Scaffold(
+  //     body: Center(
+  //       child: MyButton(),
+  //     ),
+  //   ),
+  // ));
+
+  // 4:放在一起
+  // runApp(MaterialApp(
+  //   home: Scaffold(
+  //     body: Center(
+  //       child: ShoppingListItem(
+  //         product: const Product(name: 'Chips'),
+  //         inCart: true,
+  //         onCartChanged: ((product, inCart) {}),
+  //       ),
+  //     ),
+  //   ),
+  // ));
+// 5:
   runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: MyButton(),
-      ),
+    title: 'Shopping App',
+    home: ShoppingList(
+      products: [
+        Product(name: 'Eggs'),
+        Product(name: 'Flour'),
+        Product(name: 'Chocolate chips'),
+      ],
     ),
   ));
 }
@@ -131,6 +156,101 @@ class MyButton extends StatelessWidget {
         child: const Center(
           child: Text('Engage'),
         ),
+      ),
+    );
+  }
+}
+
+// 放在一起
+class Product {
+  final String name;
+
+  const Product({required this.name});
+}
+
+typedef CartChangedCallback = Function(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  ShoppingListItem({
+    required this.product,
+    required this.inCart,
+    required this.onCartChanged,
+  }) : super(key: ObjectKey(product));
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(product.name, style: _getTextStyle(context)),
+    );
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products, super.key});
+
+  final List<Product> products;
+  @override
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      // When a user changes what's in the cart, you need
+      // to change _shoppingCart inside a setState call to
+      // trigger a rebuild.
+      // The framework then calls build, below,
+      // which updates the visual appearance of the app.
+
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
       ),
     );
   }
