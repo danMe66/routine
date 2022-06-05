a0628355335cc80b8ebaefe7ff05b1b66912ed83
Dan dan <1064265199@qq.com>
2022-06-05 17:24:25 +0800

    update .gitignore

---
+++ flutter/study/layout/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: const Center(
+          child: Text('Hello world'),
+        ),
+      ),
+    );
+  }
+}
