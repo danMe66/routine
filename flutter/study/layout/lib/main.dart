58c6a1a237aa8c55190f81d3b9936bc53839dcf4
Dan dan <1064265199@qq.com>
2022-06-06 08:20:03 +0800

    update

--- flutter/study/layout/lib/main.dart
+++ flutter/study/layout/lib/main.dart
@@ -15,10 +15,50 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello world'),
+        // body: const Center(
+        //   child: Text('Hello world'),
+        // ),
+        // body: Center(child: buildRow()),
+        // body: Center(child: buildColumn()),
+        body: Center(
+          child: Column(
+            children: [
+              buildRow(),
+              buildColumn(),
+              starRow(),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Widget buildRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(child: Image.asset('images/pic1.jpeg')),
+          // Expanded(flex: 2, child: Image.asset('images/pic2.jpeg')),
+          Expanded(child: Image.asset('images/pic3.jpeg')),
+        ],
+      );
+
+  Widget buildColumn() => Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/pic1.jpeg'),
+          Image.asset('images/pic2.jpeg'),
+          Image.asset('images/pic3.jpeg'),
+        ],
+      );
+
+  Widget starRow() => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.green[500]),
+          const Icon(Icons.star, color: Colors.black),
+          const Icon(Icons.star, color: Colors.black),
+        ],
+      );
 }
